7e76a78df20aaa0e0ebc49c3afb902d0e4ccb786
Iuliia Shmitko <75015811+juliShmi@users.noreply.github.com>
2026-07-21 17:06:39 +0300

    Add: validate-parentheses - submission-11

---
+++ Data Structures & Algorithms/validate-parentheses/submission-11.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() <=1) {
+            return false;
+        }
+
+        Map<Character, Character> map = new HashMap();
+        map.put('}', '{');
+        map.put(']', '[');
+        map.put(')', '(');
+
+        Deque<Character> stack = new LinkedList<>();
+        for (char bracket : s.toCharArray()) {
+            if (map.containsKey(bracket)) {
+                if (!stack.isEmpty() && map.get(bracket).equals(stack.peek())) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stack.push(bracket);
+        }
+    }
+        return stack.isEmpty();
+    }
+}
